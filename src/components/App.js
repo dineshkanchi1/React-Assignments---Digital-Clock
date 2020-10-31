84278a57cc295b9f268f25a46f9f8f4fe61d3cea
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-11-01 00:25:04 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React, { Component, useState } from "react";
 import "../styles/App.css";
-import Clock from "./Clock";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date()
+    };
+  }
+  count() {
+    this.setState({ time: new Date() });
+  }
+  componentDidMount() {
+    this.intervalId = setInterval(() => this.count(), 1000);
+  }
+  componentWillMount() {
+    clearInterval(this.intervalId);
+  }
   render() {
     return (
       <div className="App">
-        <Clock />
+        <div className="Clock">
+          <h3 id="time">{this.state.time}</h3>
+        </div>
       </div>
     );
   }

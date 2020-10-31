efcbfb636761168fd0375e46de8cab6cc598daaa
Dinesh Kanchi <k.d.v.kumar123@gmail.com>
2020-11-01 00:30:51 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,23 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date()
+      time: new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
     };
   }
   count() {
-    this.setState({ time: new Date() });
+    this.setState({
+      time: new Date().toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
   }
   componentDidMount() {
     this.intervalId = setInterval(() => this.count(), 1000);
